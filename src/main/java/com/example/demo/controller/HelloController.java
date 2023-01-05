a2d6cdca8eb30e9abbbb8fbc1fbac5f080e2837f
roadmonster <roadmonster1989@gmail.com>
2023-01-04 21:49:08 -0800

    update

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,25 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private String name;
     @GetMapping("/")
     public String hello(){
         return "Hello world";
+    }
+
+    @PostMapping("/{name}")
+    public void createName(@PathVariable String name){
+        this.name = name;
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(){
+        return "Hello" + this.name;
     }
 }
